e80b8e3155d0bea98c2beeac949be1945a0c33cf
Peterson <pmugasa78@gmail.com>
2022-09-15 00:03:22 +0200

    added the rounder counter

--- index.js
+++ index.js
@@ -10,30 +10,37 @@ function getComputerChoice() {
   }
 }
 
-// Score tracking
+// Score counter
 let compScore = 0;
 let playerScore = 0;
+
+//round counter
+let round = 0;
 
 //Playing round functions
 function playRound(computerSelection, playerSelection) {
   if (computerSelection === playerSelection) {
     const div = document.querySelector("#results");
     div.textContent = "Its a draw";
+    ++round;
   } else if (computerSelection === "rock" && playerSelection === "scissors") {
     //when computer wins
     ++compScore;
+    ++round;
     const compResult = document.querySelector("#comp");
     compResult.textContent = compScore;
     const div = document.querySelector("#results");
     div.textContent = "you lost";
   } else if (computerSelection === "paper" && playerSelection === "rock") {
     ++compScore;
+    ++round;
     const compResult = document.querySelector("#comp");
     compResult.textContent = compScore;
     const div = document.querySelector("#results");
     div.textContent = "you lost";
   } else if (computerSelection === "scissors" && playerSelection === "paper") {
     ++compScore;
+    ++round;
     const compResult = document.querySelector("#comp");
     compResult.textContent = compScore;
     const div = document.querySelector("#results");
@@ -41,18 +48,21 @@ function playRound(computerSelection, playerSelection) {
   } else if (computerSelection === "scissors" && playerSelection === "rock") {
     //when player wins
     ++playerScore;
+    ++round;
     const playerResult = document.querySelector("#player");
     playerResult.textContent = playerScore;
     const div = document.querySelector("#results");
     div.textContent = "you won";
   } else if (computerSelection === "paper" && playerSelection === "scissors") {
     ++playerScore;
+    ++round;
     const playerResult = document.querySelector("#player");
     playerResult.textContent = playerScore;
     const div = document.querySelector("#results");
     div.textContent = "you won";
   } else if (computerSelection === "rock" && playerSelection === "paper") {
     ++playerScore;
+    ++round;
     const playerResult = document.querySelector("#player");
     playerResult.textContent = playerScore;
     const div = document.querySelector("#results");
@@ -61,13 +71,14 @@ function playRound(computerSelection, playerSelection) {
 }
 
 //play 5 rounds of the game
-/*
+
 function game() {
   for (let i = 0; i < 5; i++) {
     playRound();
   }
 }
-*/
+
+// results after 5 rounds
 function results() {
   //determining the winner
   if (playerScore === compScore) {
@@ -83,8 +94,6 @@ function results() {
   }
 }
 
-// Rock button
-
 //getting hold of the buttons
 const buttons = document.querySelectorAll("button");
 
@@ -94,11 +103,25 @@ buttons.forEach((btn) => {
     let playerSelection = btn.getAttribute("value");
     let computerSelection = getComputerChoice();
     playRound(computerSelection, playerSelection);
+    //Round counter
+    const roundCounter = document.querySelector("#counter");
+    roundCounter.textContent = "Rounds played" + " " + round + " out of 5 ";
   });
 });
 
+//Score board
 const compResult = document.querySelector("#comp");
 compResult.textContent = compScore;
 
 const playerResult = document.querySelector("#player");
 playerResult.textContent = playerScore;
+
+//Round counter
+const roundCounter = document.querySelector("#counter");
+roundCounter.textContent = "Rounds played" + " " + round + " out of 5 ";
+
+// play 5 rounds
+// after 5 rounds evaluate the winner
+// set counter
+// when counter reaches 5 determine the winner
+// display the winner and then reset the score
